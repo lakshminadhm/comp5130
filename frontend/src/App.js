0512b27de700f375ffdc5fd9827511c2549aea11
Lakshmi Nadh <lakshminadh414@gmail.com>
2024-10-04 17:17:55 -0400

    undo changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Container, Box } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -12,38 +11,24 @@ function App() {
   return (
     <Router>
       <Container maxWidth="lg" style={{ marginTop: '50px' }}>
-
+        <Header />
         <Box
           sx={{
             backgroundColor: '#f9f9f9',
             padding: '20px',
             borderRadius: '8px',
             boxShadow: '0px 4px 12px rgba(0,0,0,0.1)',
-            height: '600px', // Fixed height for the box
-            overflowY: 'auto' // Makes the content scrollable when it exceeds the height
+            height: '600px',
+            overflowY: 'auto'
           }}
         >
-
-          {/* Header Component */}
-          <Header />
-
           <Routes>
-            {/* Homepage Route */}
             <Route path="/" element={<Homepage />} />
-
-            {/* Create Note Route */}
             <Route path="/create" element={<CreateNote />} />
-
-            {/* View Note Route - The noteId is captured as a parameter */}
             <Route path="/view/:noteId" element={<ViewNote />} />
-
-            {/* Manual Note ID Entry Route - Could use the same ViewNote component */}
             <Route path="/view" element={<ViewNote />} />
-
-            {/* Catch-all route for undefined paths */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-
         </Box>
       </Container>
     </Router>
